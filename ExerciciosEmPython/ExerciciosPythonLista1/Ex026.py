27126d469fcce673300ac5492b8d3069ef2daac2
Davi Oliveira <davioliveiraes7@gmail.com>
2025-03-23 14:37:54 -0300

    Project update on Linux

---
+++ ExerciciosEmPython/ExerciciosPythonLista1/Ex026.py
@@ -0,0 +1,11 @@
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite outro número: "))
+
+if numero_1 > numero_2:
+   print("O primeiro valor é o maior!")
+elif numero_2 > numero_1:
+   print("O segundo valor é o maior!")
+else:
+   print("Não existe valor maior, os dois são iguais!")
+
+
